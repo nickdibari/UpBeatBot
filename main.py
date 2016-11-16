3be76125d839ec0822e766dce15d000b9afb59c6
nickdibari <ndibari@fordham.edu>
2016-11-16 11:34:15 -0500

    Logging Implementation

--- main.py
+++ main.py
@@ -5,12 +5,13 @@
 # Twitter bot to tweet uplifting things
 
 import bs4
+import logging
 import random
 import requests
 import twitter
 import time
 
-from auth_example import *
+from twiter_auth import *
 
 # PRE: N/A
 # POST: Connection to twitter API
@@ -26,7 +27,7 @@ def GetImage():
 	animals = ['kittens','pugs','cats','gerbils','bunnies','chipmunks','dogs','otters']
 	animal = random.choice(animals)
 
-	print('Gonna get a picture of {0}'.format(animal))
+	logging.info(' Gonna get a picture of {0}'.format(animal))
 	# Get preview page for random animal
 	PreHTML = requests.get('http://www.cutestpaw.com/?s={0}'.format(animal))
 	PreHTML.raise_for_status()
@@ -34,14 +35,14 @@ def GetImage():
 	PreObject = bs4.BeautifulSoup(PreHTML.text, 'html.parser')
 
 	if PreObject:
-		print('Got preview page OK')
+		logging.info(' Got preview page OK')
 
 	# Get random picture from preview page
 	photos = PreObject.select('#photos a')
 	choice = random.choice(photos)
 
 	if choice:
-		print('Got choice of picture OK')
+		logging.info(' Got choice of picture OK')
 
 	# Get picture page
 	PicHTML = requests.get(choice['href'])
@@ -50,51 +51,56 @@ def GetImage():
 	PicObject = bs4.BeautifulSoup(PicHTML.text, 'html.parser')
 
 	if PicObject:
-		print('Got picture page OK')
+		logging.info(' Got picture page OK')
 
 	# Parse picture page for image
 	img = PicObject.select('#single-cute-wrap img')
 	link =  img[0]['src']
 
 	if link:
-		print('Got image link OK')
+		logging.info(' Got image link OK')
+	
 	return link
 
 # Main Driver
 def main():
+    logging.basicConfig(filename='dev.log', level=logging.DEBUG)
+
 	conx = ConnectAPI()
 	
 	if conx:
-		print('Connected to Twitter OK')
+		logging.info(' Connected to API OK')
 	
 	while True:
 		mentions = conx.GetMentions()
 		
 		if mentions:
-			print('Got {0} mentions'.format(len(mentions)))
+			logging.info(' Got {0} mentions'.format(len(mentions)))
 			for mention in mentions:
 				user = mention.user.screen_name
 
 				if not mention.favorited:
+					logging.info(' Gonna tweet @{0}'.format(user))
+
 					text = 'Hey @{0}, hope this brightens your day!'.format(user)
 					img = GetImage()
 
 					status = conx.PostUpdate(text,img)
 
 					if status:
-						print('Tweeted at {0} OK'.format(user))
+						logging.info(' Tweeted at {0} OK'.format(user))
 					
 					conx.CreateFavorite(status=mention)
 
 				else:
-					print('Already tweeted @{0}'.format(user))
+					logging.info(' Already tweeted @{0}'.format(user))
 
 		else:
-			print('Got no mentions')
+			logging.info(' Got no mentions')
 
-		print('Going to sleep..')
+		logging.info(' Going to sleep..')
 		time.sleep(60)
-		print('Waking up!')
+		logging.info(' Waking up!')
 
 if __name__ == '__main__':
 	main()
