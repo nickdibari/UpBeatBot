43dce889a79b77445eebe0d0e15532b64e7728d5
nickdibari <ndibari@fordham.edu>
2019-03-25 23:39:35 -0400

    Use more descriptive unit test names

--- tests/test_upbeatbot.py
+++ tests/test_upbeatbot.py
@@ -8,20 +8,20 @@ class TestUpbeatBot(unittest.TestCase):
     def setUpClass(cls):
         cls.upbeat_bot = UpBeatBot()
 
-    def test_chosen_animal_returned(self):
+    def test_get_animal_from_message_chosen_animal_returned(self):
         tweet = 'Hey @upbeatbot send me a dog!'
         animal = self.upbeat_bot._get_animal_from_message(tweet)
 
         self.assertEqual(animal, 'dog')
 
-    def test_random_animal_returned_with_text(self):
+    def test__get_animal_from_message_random_animal_returned_with_text(self):
         tweet = 'Hey @upbeatbot send me a pic!'
         animal = self.upbeat_bot._get_animal_from_message(tweet)
 
         # Not really a test, just ensuring *something* is returned
         self.assertTrue(animal)
 
-    def test_random_returned_no_text(self):
+    def test__get_animal_from_message_random_returned_no_text(self):
         tweet = '@upbeatbot'  # Minimum viable string
         animal = self.upbeat_bot._get_animal_from_message(tweet)
 
