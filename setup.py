85fc81bf0c609f3852485888cb0d11f764dfe378
nickdibari <ndibari@fordham.edu>
2020-02-27 22:47:36 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="upbeatbot",
+    version="0.0.1",
+    author="Nicholas DIbari",
+    author_email="ndibari@fordham.edu",
+    description="Twitter bot to tweet uplifting images at twitter users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nickdibari/UpBeatBot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
